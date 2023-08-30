3263c4e0bee3221d8158934ba55c2dc40311ed7c
erlosshex <gugsabr@gmail.com>
2023-08-30 22:11:06 +0800

    添加qingp_machinevision

---
+++ machine_vision/QingP_MachineVision/QingP_MachineVision/main.cpp
@@ -0,0 +1,11 @@
+#include <QtCore/QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "Hello, World!";
+    qDebug() << "Hello, QingP Machine Vision!";
+
+    return a.exec();
+}
