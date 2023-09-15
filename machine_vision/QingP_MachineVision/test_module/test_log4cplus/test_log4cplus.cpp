0544285cfb7ee5f21f58891d593cb7eaa7c8c1f9
erlosshex <gugsabr@gmail.com>
2023-09-15 23:52:06 +0800

    学习log4cplus第一步

---
+++ machine_vision/QingP_MachineVision/test_module/test_log4cplus/test_log4cplus.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#include <log4cplus/log4cplus.h>
+
+int main()
+{
+    log4cplus::Initializer logInitializer;
+
+    log4cplus::BasicConfigurator logConfig;
+    logConfig.configure();
+
+    log4cplus::Logger logLogger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("main"));
+    LOG4CPLUS_WARN(logLogger, LOG4CPLUS_TEXT("Hello, Logger World!"));
+
+    system("pause");
+    return 0;
+}
+
+
+
