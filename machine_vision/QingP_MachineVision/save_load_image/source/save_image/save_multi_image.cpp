c05f84b4ccb6a7cd1da6938f6cf44b03953e844d
erlosshex <gugsabr@gmail.com>
2023-09-26 22:28:01 +0800

    创建pytorch测试环境，保存多张图片类

---
+++ machine_vision/QingP_MachineVision/save_load_image/source/save_image/save_multi_image.cpp
@@ -0,0 +1,71 @@
+#include "save_one_image.h"
+
+#include "../../../common_tool/source/file_operator/FileOperator.h"
+
+SaveOneImage::SaveOneImage()
+{
+
+}
+
+SaveOneImage::~SaveOneImage()
+{
+
+}
+
+uint SaveOneImage::saveImage(SaveOneImageResult& stResult)
+{
+	try
+	{
+		stResult.bIsOK = false;
+		stResult.strImagePath = "";
+		stResult.strImageName = "";
+		stResult.strImagePathname = "";
+
+		QString strImagePathname = m_stParam.strImagePathname;
+		if (strImagePathname == "")
+		{
+			strImagePathname = m_stParam.strImagePath + m_stParam.strImageName;
+		}
+
+		if (strImagePathname == "")
+		{
+			return SAVEONEIMAGE_COMMON_PARAERROR;
+		}
+
+		QString strImagePath = m_stParam.strImagePath;
+		QString strImageName = m_stParam.strImageName;
+		if (strImagePath == "" || strImageName == "")
+		{
+			QStringList lststrSplitFilePathname = CommonTool_FileOperator::splitFilePathname(strImagePathname);
+			if (lststrSplitFilePathname.size() == 2)
+			{
+				strImagePath = lststrSplitFilePathname[0];
+				strImageName = lststrSplitFilePathname[1];
+			}
+		}
+
+		if (strImagePath == "" || strImageName == "")
+		{
+			return SAVEONEIMAGE_COMMON_PARAERROR;
+		}
+
+		CommonTool_FileOperator::createPath(strImagePath);
+
+		if (false == CommonTool_FileOperator::isDirExist(strImagePath))
+		{
+			return SAVEONEIMAGE_COMMON_PARAERROR;
+		}
+
+		stResult.bIsOK = cv::imwrite(strImagePathname.toLocal8Bit().toStdString().c_str(), m_stParam.cvmImage);
+
+		stResult.strImagePath = strImagePath;
+		stResult.strImageName = strImageName;
+		stResult.strImagePathname = strImagePathname;
+
+		return SAVEONEIMAGE_COMMON_OK;
+	}
+	catch (std::exception& ex)
+	{
+		return SAVEONEIMAGE_COMMON_UNKNOWERROR;
+	}
+}
---
+++ machine_vision/QingP_MachineVision/save_load_image/source/save_image/save_multi_image.h
@@ -0,0 +1,50 @@
+﻿#ifndef SAVELOADIMAGE_SAVEONEIMAGE_H
+#define SAVELOADIMAGE_SAVEONEIMAGE_H
+
+#include "../../save_load_image_global.h"
+
+#include <QObject>
+
+#include <QObject>
+#include <QString>
+
+#include <opencv2/opencv.hpp>
+
+#include "../save_load_image_common/save_load_image_common_define.h"
+
+class SAVE_LOAD_IMAGE_EXPORT SaveOneImage : public QObject
+{
+    Q_OBJECT
+public:
+    SaveOneImage();
+    ~SaveOneImage();
+
+    struct SaveOneImageParam
+    {
+        QString strImagePath = QString("");
+        QString strImageName = QString("");
+        QString strImagePathname = QString("");
+
+        cv::Mat cvmImage = cv::Mat();
+    };
+
+    struct SaveOneImageResult
+    {
+        bool bIsOK = false;
+
+        QString strImagePath = QString("");
+        QString strImageName = QString("");
+        QString strImagePathname = QString("");
+    };
+
+    SaveOneImageParam m_stParam;
+
+    /// <summary>
+    /// 保存图片
+    /// </summary>
+    /// <param name="stResult">返回值，保存图片后的结果</param>
+    /// <returns>错误代码</returns>
+    uint saveImage(SaveOneImageResult& stResult);
+};
+
+#endif
